f560be7af69cc9f90978d55ea77435dd4b64229b
Suraj_SJ <jaiswaljesus384@gmail.com>
2022-03-12 20:54:41 +0530

    yes

---
+++ notes_partial/_nav.php
@@ -0,0 +1,48 @@
+<?php
+echo '  <div class="header-nav ">
+        <nav class="navbar navbar-expand-lg navbar-light ">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <a class="navbar-brand px-3" href="#"><h2>Edu-change</h2></a>
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="/PHPT/Edu-change/index.html">Home</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="/PHPT/Edu-change/index.html">Manual&Record</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact</a>
+                        </li>
+
+                    </ul>
+
+                    <!-- Login Button -->
+                    <a href="_login.php"><button type="button" class="btn btn-warning m-2">Login</button></a>
+
+                    <!-- SignUp Button -->
+                    <a href="#"><button type="button" class="btn btn-warning m-2">SignUp</button></a>
+
+                    <!-- Teacher SignUp Button -->
+                    <a href="_teacher_signup.php"><button type="button" class="btn btn-danger m-2">Teacher SignUp</button></a>
+
+
+                </div>
+            </div>
+        </nav>
+    </div>
+
+';
+?>
+
